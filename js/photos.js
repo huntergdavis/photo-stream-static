e1395515f167021d0a406cc20eee1e0e91870ade
Hunter Davis <hunter@hunterdavis.com>
2020-03-11 15:17:12 -0700

    updating baseurl

--- js/photos.js
+++ js/photos.js
@@ -6,13 +6,13 @@ container.innerHTML = html;
 document.querySelector(`#${photo}`).classList.add(TARGET_CLASS);
 lazyload();
 })(`
-<li class="item " id="munchie-riding-shotgun" style="background-image: url('/photos/tint/munchie%20riding%20shotgun-65ba1d.jpg')" title="munchie riding shotgun">
-  <img class="lazyload" data-src="/photos/thumbnail/munchie%20riding%20shotgun-ccf4df.jpg" src="/photos/tint/munchie%20riding%20shotgun-65ba1d.jpg" height="3024" width="4032" />
+<li class="item " id="munchie-riding-shotgun" style="background-image: url('/photo-stream/photos/tint/munchie%20riding%20shotgun-65ba1d.jpg')" title="munchie riding shotgun">
+  <img class="lazyload" data-src="/photo-stream/photos/thumbnail/munchie%20riding%20shotgun-ccf4df.jpg" src="/photo-stream/photos/tint/munchie%20riding%20shotgun-65ba1d.jpg" height="3024" width="4032" />
   <span class="full">
-    <span style="background-image: url('/photos/large/munchie%20riding%20shotgun-686656.jpg')"></span>
+    <span style="background-image: url('/photo-stream/photos/large/munchie%20riding%20shotgun-686656.jpg')"></span>
   </span>
-  <a class="open" href="/munchie-riding-shotgun" data-target="munchie-riding-shotgun">Open</a>
-  <a class="close" href="/">Close</a>
+  <a class="open" href="/photo-stream/munchie-riding-shotgun" data-target="munchie-riding-shotgun">Open</a>
+  <a class="close" href="/photo-stream/">Close</a>
   <!-- <ul class="meta">
     <li></li>
     <li>1/144</li>
